7a3572078377afcca69b7d78fecf52d1e05f046c
kanajakala <xiron.kanajakala@gmail.com>
2024-08-31 16:39:00 +0200

    added new lines and boundary checks when printing

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -22,8 +22,9 @@ const font: Font = .{
 
 var framebuffer: *limine.Framebuffer = undefined;
 
-//this variable keeps track of where the character needs to be put
-var cursor: usize = 0;
+//variables to help with character placement
+var col: usize = 0;
+var row: usize = 0;
 
 inline fn stop() noreturn {
     while (true) {
@@ -55,36 +56,47 @@ inline fn dprint(comptime string: [:0]const u8) void {
 
 fn putpixel(x: usize, y: usize, color: u32) void {
     // Calculate the pixel offset using the framebuffer information we obtained above.
-    // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+    // We skip `y` scanlines (pitch is provided in bytes) and add `x * 4` to skip `x` pixels forward.
     const pixel_offset = y * framebuffer.pitch + x * 4;
 
     // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
     @as(*u32, @ptrCast(@alignCast(framebuffer.address + pixel_offset))).* = color;
 }
 
-fn drawCharFull(char: u16, x: usize, y: usize, fg: u32, bg: u32) void {
+fn printChar(char: u16, fg: u32, bg: u32) void {
     //see https://wiki.osdev.org/VGA_Fonts#Decoding_of_bitmap_fonts
     const mask = [8]u8{ 1, 2, 4, 8, 16, 32, 64, 128 };
     const glyph_offset: usize = char * font.height;
-    for (0..font.width) |cx| {
-        for (0..font.height) |cy| {
-            const pixel_color = if (font.data[glyph_offset + cy] & mask[cx] != 0) fg else bg;
-            putpixel((font.width - cx) + x, cy + y, pixel_color);
+    if (char == '\n') {
+        newLine();
+    } else if ((col + font.width) < framebuffer.width) {
+        for (0..font.width) |cx| {
+            for (0..font.height) |cy| {
+                if (bg == 0x000000) {
+                    if (font.data[glyph_offset + cy] & mask[cx] != 0) putpixel((font.width - cx) + col, cy + row, fg);
+                } else {
+                    const pixel_color = if (font.data[glyph_offset + cy] & mask[cx] != 0) fg else bg;
+                    putpixel((font.width - cx) + col, cy + row, pixel_color);
+                }
+            }
         }
+        col += font.width;
+    } else if ((row + font.height) < framebuffer.height) {
+        newLine();
+    } else {
+        row = 0;
+        col = 0;
     }
 }
 
-fn drawStringFull(string: [:0]const u8, x: usize, y: usize, fg: u32, bg: u32) void {
-    for (string, 0..string.len) |char, offset| {
-        drawCharFull(char, x + (offset * font.width), y, fg, bg);
-    }
+fn newLine() void {
+    row += font.height;
+    col = 0;
 }
 
-inline fn loopTest() void {
-    var i: usize = 0;
-    var t: usize = 0;
-    while (i < 1000) : (i += 1) {
-        t += 10;
+fn print(string: [:0]const u8, fg: u32, bg: u32) void {
+    for (string) |char| {
+        printChar(char, fg, bg);
     }
 }
 
@@ -105,8 +117,16 @@ export fn _start() callconv(.C) noreturn {
     }
     // Get the first framebuffer's information.
     putpixel(0, 0, 0xFFFFFFFF);
-    drawStringFull("Welcome to CaCOS", 0, 0, @intCast(0xFFFFFF), @intCast(0x000000));
-    loopTest();
+    print(" .d8888b.            .d8888b.   .d88888b.   .d8888b.  \n", 0xf6aa70, 0x0);
+    print("d88P  Y88b          d88P  Y88b d88P\" \"Y88b d88P  Y88b \n", 0xf6aa70, 0x0);
+    print("888    888          888    888 888     888 Y88b.      \n", 0xf6aa70, 0x0);
+    print("888         8888b.  888        888     888  \"Y888b.   \n", 0xf6aa70, 0x0);
+    print("888            \"88b 888        888     888     \"Y88b. \n", 0xf6aa70, 0x0);
+    print("888    888 .d888888 888    888 888     888       \"888 \n", 0xf6aa70, 0x0);
+    print("Y88b  d88P 888  888 Y88b  d88P Y88b. .d88P Y88b  d88P \n", 0xf6aa70, 0x0);
+    print(" \"Y8888P\"  \"Y888888  \"Y8888P\"   \"Y88888P\"   \"Y8888P\"  \n", 0xf6aa70, 0x0);
+
+    print("Welocome to CaCOS\n", @intCast(0xFFFFFF), @intCast(0x000000));
 
     while (true) {}
 }
